8c289900dccc955ffb8720b2a86d75e1dd9744f5
bbbence <benceborbas88@gmail.com>
2021-02-22 23:40:57 +0100

    Product class created.

---
+++ src/app/model/product.ts
@@ -0,0 +1,12 @@
+export class Product {
+    id: number = 0;
+    name: string = '';
+    type: string = '';
+    catID: number = 0;
+    description: string = '';
+    price: number = 0;
+    featured: boolean;
+    active: boolean;
+}
+
+// id, name, type, catID, description, price, featured, active
