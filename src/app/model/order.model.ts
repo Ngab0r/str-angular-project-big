33b6dfc4bf31ea6d2d20fa20e07a003c2d65f028
ditta2020 <illyesedit@protonmail.com>
2021-02-24 09:17:58 +0100

    add Order class

---
+++ src/app/model/order.model.ts
@@ -0,0 +1,13 @@
+enum OrderStatus {
+    new,
+    shipped,
+    paid
+}
+
+export class Order {
+    id:number = 0;
+    customerID: number = 0;
+    productID: number = 0;
+    amount: number = 0;
+    status: OrderStatus = OrderStatus.new;
+}
