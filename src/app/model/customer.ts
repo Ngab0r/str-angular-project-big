7415dd87e9e418574a78c41575eb6e52a4e8e212
Nemcsics Gábor <nemcsics.gabor@gmail.com>
2021-02-24 18:39:31 +0100

    customer.ts address bugfix

--- src/app/model/customer.ts
+++ src/app/model/customer.ts
@@ -1,38 +1,38 @@
-import { Address } from 'src/app/model/address';
+import { Address } from '../model/address';
 
 export class CustomerSimple {
-    id:number = 0;
-    firstName:string = '';
-    lastName:string = '';
-    email:string = '';
-    address:Address = new Address();
-    active:boolean = false;
+    id: number = 0;
+    firstName: string = '';
+    lastName: string = '';
+    email: string = '';
+    address: Address = new Address();
+    active: boolean = false;
 }
 
 export interface ICustomer {
     id: number;
-    firstName:string;
-    lastName:string;
-    email:string;
-    address:Address;
-    active:boolean;
+    firstName: string;
+    lastName: string;
+    email: string;
+    address: Address;
+    active: boolean;
 
     [others: string]: any;
 }
 
-export class Customer implements ICustomer{
+export class Customer implements ICustomer {
     id: number;
-    firstName:string;
-    lastName:string;
-    email:string;
-    address:Address;
-    active:boolean;
+    firstName: string;
+    lastName: string;
+    email: string;
+    address: Address;
+    active: boolean;
     constructor(customer: Partial<ICustomer>) {
         this.id = typeof customer.id === 'number' ? customer.id : 0;
         this.firstName = typeof customer.firstName === 'string' ? customer.firstName : '';
         this.lastName = typeof customer.lastName === 'string' ? customer.lastName : '';
         this.email = typeof customer.email === 'string' ? customer.email : '';
-        this.address = typeof customer.address === 'undefined' ? new Address() : new Address(customer.address);
+        this.address = typeof customer.address === 'undefined' ? new Address() : customer.address;
         this.active = typeof customer.active === 'boolean' ? customer.active : false;
     }
 }
