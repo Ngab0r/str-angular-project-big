dfe7d610035fab6a948714eb6fefdf444b4e6ec6
Nemcsics Gábor <nemcsics.gabor@gmail.com>
2021-03-02 21:56:50 +0100

    add plus button to productlist and add delete button and delete modal and sorter working again

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -9,6 +9,7 @@ import { Sorter } from 'app/model/sorter';
 import { MatTableDataSource } from '@angular/material/table';
 import { CdkDragDrop } from '@angular/cdk/drag-drop';
 import { FilterPipe } from '../pipe/filter.pipe';
+import { SorterPipe } from 'app/pipe/sorter.pipe';
 
 @Component({
   selector: 'app-product',
@@ -26,50 +27,8 @@ export class ProductComponent implements OnInit {
   // 2.) - Observable
   //addressList: Observable<Address[]>;
 
-
-  // displayedColumns = ['id', 'name', 'age', 'gender', 'country'];
-  rows = [
-    {
-      id: '1',
-      name: 'John',
-      age: '21',
-      gender: 'Male',
-      country: 'UK'
-    },
-    {
-      id: '2',
-      name: 'Robin',
-      age: '25',
-      gender: 'Male',
-      country: 'London'
-    },
-    {
-      id: '3',
-      name: 'Robert',
-      age: '12',
-      gender: 'Male',
-      country: 'Dubai'
-    },
-    {
-      id: '4',
-      name: 'Neeraj',
-      age: '23',
-      gender: 'Male',
-      country: 'India'
-    },
-    {
-      id: '5',
-      name: 'Wiliiams',
-      age: '30',
-      gender: 'Male',
-      country: 'Ausralia'
-    }
-  ];
-
-  displayedColumns = ['image', 'id', 'name', 'title', 'year', 'type', 'catID', 'description', 'price', 'active', 'edit'];
+  displayedColumns = ['image', 'id', 'name', 'title', 'year', 'type', 'catID', 'description', 'price', 'active', 'edit', 'delete'];
   columns: any[] = [
-
-
     {
       name: 'id',
       title: 'No.'
@@ -108,62 +67,50 @@ export class ProductComponent implements OnInit {
     },
   ];
   filterPipe: FilterPipe = new FilterPipe();
+  sorterPipe: SorterPipe = new SorterPipe();
+  subscribeForDeleteItem: Product = new Product();
 
   constructor(
     private productService: ProductService,
-
   ) {
     // 2.) - Observable  
     //  this.addressX = this.productService.getAll();
   }
 
   ngOnInit(): void {
-    // this.dataSource = new MatTableDataSource(this.rows);
     this.productService.getAll();
     this.productList$.subscribe(list => {
       this.dataSource = new MatTableDataSource(list);
-      this.changeFilterPredicate();
     });
-    console.log(this.dataSource);
-
-    // this.dataSource.filterPredicate = function (data, filter: string): boolean {
-    //   var dataa = this.productList$.type.filter(
-    //     element => element.position === data.position);
-    //   return data.position.toString().toLowerCase().includes(filter)
-    //     || data.symbol.toLowerCase().includes(filter)
-    //     || data.position.toString().toLowerCase() === filter
-    //     || dataa[0].name.toString().toLowerCase().includes(filter.toLowerCase());
-    // };
-    // console.log(this.dataSource);
     this.dataSource.filter = 'abba';
     this.dataSource.filter = 'abba';
   }
 
+  subscribeForDelete(subscribeForDeleteItem: Product): void {
+    this.subscribeForDeleteItem = subscribeForDeleteItem;
+  }
+
+  delete(): void {
+    this.productService.remove(this.subscribeForDeleteItem);
+  }
 
   changeFilter(filter: Filter): void {
-    console.log(filter.phrase);
-    console.log(filter.selectedKeyForSearch);
     this.filter = filter;
     this.productList$.subscribe(list => {
       this.dataSource = new MatTableDataSource(this.filterPipe.transform(list, this.filter.phrase, this.filter.selectedKeyForSearch, this.filter.phrase2));
-      // this.changeFilterPredicate();
     });
-
-    // this.changeFilterPredicate();
-    // this.dataSource.filter = this.filter.phrase;
-    // console.log(this.dataSource);
-
   }
 
   selectColumnForSort(col: string): void {
     this.sorter.sortKey === col ? this.sorter.sortAscend = !this.sorter.sortAscend : this.sorter.sortAscend = true;
     this.sorter.sortKey = col;
+    this.productList$.subscribe(list => {
+      this.dataSource = new MatTableDataSource(this.sorterPipe.transform(list, this.sorter.sortKey, this.sorter.sortAscend));
+    });
   }
 
   tableDrop(event: CdkDragDrop<string[]>) {
-    // if (event.currentIndex !== this.displayedColumns.length && event.currentIndex !== 0) {
     this.displayedColumns = this.moveItemInArray(this.displayedColumns, event.previousIndex + 1, event.currentIndex + 1);
-    // }
   }
 
   moveItemInArray(array: any[], prev: number, curr: number) {
@@ -175,38 +122,6 @@ export class ProductComponent implements OnInit {
     }
     array.splice(curr, 0, array.splice(prev, 1)[0]);
     return array
-  }
-
-
-  changeFilterPredicate() {
-
-    if (this.filter.selectedKeyForSearch === 'price') {
-      if (!isNaN(parseInt(this.filter.phrase)) || !isNaN(parseInt(this.filter.phrase2))) {
-        if (!isNaN(parseInt(this.filter.phrase)) && !isNaN(parseInt(this.filter.phrase2))) {
-          console.log(this.filter.selectedKeyForSearch, 'juhé');
-          this.dataSource.filterPredicate =
-            (data: Product, filter: string) =>
-              data[this.filter.selectedKeyForSearch].filter(item => parseInt(this.filter.phrase) <= parseInt(item[this.filter.selectedKeyForSearch]) && parseInt(this.filter.phrase2) >= parseInt(item[this.filter.selectedKeyForSearch]));;
-        }
-        if (!isNaN(parseInt(this.filter.phrase))) {
-          this.dataSource.filterPredicate =
-            (data: Product, filter: string) =>
-              data[this.filter.selectedKeyForSearch].filter(item => parseInt(this.filter.phrase) <= parseInt(item[this.filter.selectedKeyForSearch]));
-        }
-        if (!isNaN(parseInt(this.filter.phrase2))) {
-          this.dataSource.filterPredicate =
-            (data: Product, filter: string) =>
-              data[this.filter.selectedKeyForSearch].filter(item => parseInt(this.filter.phrase2) >= parseInt(item[this.filter.selectedKeyForSearch]));
-        }
-      }
-      else {
-        this.dataSource.filterPredicate =
-          (data: Product, filter: string) => data[this.filter.selectedKeyForSearch];
-      }
-    }
-
-    this.dataSource.filterPredicate =
-      (data: Product, filter: string) => data[this.filter.selectedKeyForSearch].toString().toLowerCase().includes(filter.toLowerCase());
   }
 
 }
