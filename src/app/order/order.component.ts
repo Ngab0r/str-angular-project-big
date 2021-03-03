6f9970cc7e6010a76d16a0a3b46ad77c367ef5fd
Nemcsics Gábor <nemcsics.gabor@gmail.com>
2021-03-03 22:01:58 +0100

    orderlist page complete, change list page css button icon left margin

--- src/app/order/order.component.ts
+++ src/app/order/order.component.ts
@@ -3,7 +3,13 @@ import { Component, OnInit } from '@angular/core';
 import { BehaviorSubject, Observable } from 'rxjs';
 import { OrderService } from '../service/order.service';
 import { Order } from '../model/order';
-import { Product } from 'app/model/product';
+import { FilterPipe } from 'app/pipe/filter.pipe';
+import { SorterPipe } from 'app/pipe/sorter.pipe';
+import { ToastrService } from 'ngx-toastr';
+import { Filter } from 'app/model/filter';
+import { Sorter } from 'app/model/sorter';
+import { MatTableDataSource } from '@angular/material/table';
+import { CdkDragDrop } from '@angular/cdk/drag-drop';
 
 @Component({
   selector: 'app-order',
@@ -13,11 +19,110 @@ import { Product } from 'app/model/product';
 export class OrderComponent implements OnInit {
 
   orderList$: BehaviorSubject<Order[]> = this.orderService.list$;
-  
-  constructor(private orderService: OrderService,) {}
+
+
+  filter: Filter = new Filter();
+  sorter: Sorter = new Sorter();
+  dataSource: any;
+  // 2.) - Observable
+  //addressList: Observable<Address[]>;
+
+  displayedColumns = ['id', 'customerID', 'productID', 'quantity', 'amount', 'status', 'edit', 'delete'];
+  columns: any[] = [
+    {
+      name: 'id',
+      title: 'No.'
+    },
+    {
+      name: 'customerID',
+      title: 'Customer ID'
+    },
+    {
+      name: 'productID',
+      title: 'Product Id'
+    },
+    {
+      name: 'quantity',
+      title: 'quantity'
+    },
+    {
+      name: 'amount',
+      title: 'Amount'
+    },
+    {
+      name: 'status',
+      title: 'status'
+    },
+  ];
+  filterPipe: FilterPipe = new FilterPipe();
+  sorterPipe: SorterPipe = new SorterPipe();
+  subscribeForDeleteItem: Order = new Order();
+  orderList: Order[];
+  constructor(
+    private orderService: OrderService,
+    private toastr: ToastrService,
+  ) {
+    // 2.) - Observable  
+    //  this.addressX = this.orderService.getAll();
+  }
 
   ngOnInit(): void {
+    this.filter.selectedKeyForSearch = 'id';
+    this.sorter.sortKey = 'id';
     this.orderService.getAll();
+    this.orderList$.subscribe(list => {
+      this.orderList = list;
+      this.dataSource = new MatTableDataSource(list);
+    });
   }
+
+  subscribeForDelete(subscribeForDeleteItem: Order): void {
+    this.subscribeForDeleteItem = subscribeForDeleteItem;
+  }
+
+  delete(): void {
+    this.orderService.remove(this.subscribeForDeleteItem);
+    this.toastr.success('Succesfully deleted!', 'Editor message:');
+  }
+
+  changeFilter(filter: Filter): void {
+    this.filter = filter;
+    this.orderList$.subscribe(list => {
+      this.orderList = this.filterPipe.transform(list, this.filter.phrase, this.filter.selectedKeyForSearch, this.filter.phrase2);
+      this.dataSource = new MatTableDataSource(this.orderList);
+      this.sorting();
+    });
+  }
+
+  selectColumnForSort(col: string): void {
+    if (col === 'productID' || col === 'quantity') { return };
+    this.sorter.sortKey === col ? this.sorter.sortAscend = !this.sorter.sortAscend : this.sorter.sortAscend = true;
+    this.sorter.sortKey = col;
+    this.sorting();
+  }
+  sorting() {
+    this.dataSource = new MatTableDataSource(this.sorterPipe.transform(this.orderList, this.sorter.sortKey, this.sorter.sortAscend));
+  }
+
+  tableDrop(event: CdkDragDrop<string[]>) {
+    this.displayedColumns = this.moveItemInArray(this.displayedColumns, event.previousIndex + 1, event.currentIndex + 1);
+  }
+
+  moveItemInArray(array: any[], prev: number, curr: number) {
+    if (curr >= array.length) {
+      let k = curr - array.length + 1;
+      while (k--) {
+        array.push(undefined);
+      }
+    }
+    array.splice(curr, 0, array.splice(prev, 1)[0]);
+    return array
+  }
+
+  // returnVariablesFromColumnName(element: Order, name: string): string {
+  //   const arr = name.split('.')[0].split('[')[0];
+  //   const key = 'element' + "['" + arr + "']" + name.replace(arr, '');
+  //   return (eval(key));
+  // }
 
 }
